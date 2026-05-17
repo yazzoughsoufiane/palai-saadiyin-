20e8b5b865789dd1087accfcc0902041e6a9889f
yazzoughsoufiane <soufianeyazzough9@mail.com>
2026-05-17 18:14:38 -0300

    Set hero image to shop photo

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -23,7 +23,7 @@ export default function HomeHero() {
         aria-hidden="true"
       >
         <Image
-          src="/rugs/ps-2024-003-primary.svg"
+          src="${process.env.NEXT_PUBLIC_BASE_PATH || ""}/rugs/ps-hero-primary.jpg"
           alt=""
           fill
           className="object-cover"

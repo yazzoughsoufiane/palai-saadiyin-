e539871cd7810f7c81a96ee5cbe9e71fc5d3c4a1
yazzoughsoufiane <soufianeyazzough9@mail.com>
2026-05-22 19:11:56 -0300

    Fix GitHub Pages base path

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/palai-saadiyin-',
+  assetPrefix: '/palai-saadiyin-',
+}
+
+export default nextConfig

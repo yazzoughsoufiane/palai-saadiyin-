f4fffa5abc69d345fb3df95ee31563bf3e7681f3
yazzoughsoufiane <soufianeyazzough9@mail.com>
2026-05-23 00:38:33 -0300

    remove basePath - using custom domain

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/palai-saadiyin-',
-  assetPrefix: '/palai-saadiyin-',
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: '/palai-saadiyin-',
   },
 }
 
